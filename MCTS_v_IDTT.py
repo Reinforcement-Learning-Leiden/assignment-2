219bb99022ca9949c070dda494e586e450ea2c9b
Ximis24 <61054696+Ximis24@users.noreply.github.com>
2020-03-27 10:44:46 +0100

    cp added to inputs for MCTS

--- MCTS_v_IDTT.py
+++ MCTS_v_IDTT.py
@@ -10,15 +10,14 @@ from hex_skeleton import HexBoard
 
 ### GLOBALS ###
 #BLUE_SEC_TO_THINK: int = 1
-RED_SEC_TO_THINK: int = 3
-NUMBER_OF_GAMES: int = 120
-itermax = 3000 # number of iterations for the MCTS algorithm
-MAX_SECONDS_MCTS = None #Choose the number of seconds MCTS is allowed to search, else None
+RED_SEC_TO_THINK: int = 5
+NUMBER_OF_GAMES: int = 12
+itermax = 3000 # number of iterations for the MCTS algorithm. If MAX_SECONDS_MCTS != None this is ignored
+MAX_SECONDS_MCTS = 5 #Choose the number of seconds MCTS is allowed to search, else None
 Cp=1
 ###############
 start_time=time.time()
-bWins=0
-rWins=0
+
 def main():
     board = HexBoard(boardSize) #BOARD_SIZE_TTID
     num_of_cells = board.get_board_size() * board.get_board_size()
@@ -35,7 +34,7 @@ def main():
             board.print()
             return "blue"
         move_red = tt.iterative_deepening(
-            board, is_max=True, max_seconds=RED_SEC_TO_THINK, depth_lim=15, show_AI=False)
+            board, is_max=False, max_seconds=RED_SEC_TO_THINK, depth_lim=15, show_AI=False)
         board.place(move_red, board.RED)
         board.print()
         if board.is_game_over():
@@ -64,12 +63,11 @@ if __name__ == '__main__':
     for match in range(NUMBER_OF_GAMES):  # Play 12 games
         res = main()  # Main game loop
         if res == "blue":  # If blue won
-            bWins = +1
+
             blue, red = ts.rate_1vs1(blue, red)
 
         elif res == "red":  # If red won
             red, blue = ts.rate_1vs1(red, blue)
-            rWins = +1
 
             blue, red = ts.rate_1vs1(blue, red, drawn=True)
 
@@ -79,7 +77,6 @@ if __name__ == '__main__':
 
 
     print('time to complete: '+ str(time.time()-start_time))
-    print('out of '+str(NUMBER_OF_GAMES)+" Blue won: "+str(bWins)+" and Red won: "+str(rWins)+".")
     print("BLUE'S RANK: " + str(blue.mu))
     print("RED'S RANK: " + str(red.mu))
     plt.plot(to_plot_x, to_plot_blue)
